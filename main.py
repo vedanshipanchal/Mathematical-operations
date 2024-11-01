583f71d676f267596d641760ac03f6727ffeb858
vedanshipanchal <vedanshipanchal@gmail.com>
2024-11-01 16:49:26 +0000

    Mathematical operations

---
+++ main.py
@@ -0,0 +1,19 @@
+import math
+
+power = math.pow(2,5)
+print(power)
+
+flo = math.floor(43.87)
+print(flo)
+
+ce = math.ceil(43.87)
+print(ce)
+
+fa = math.fabs(-5)
+print(fa)
+
+fact = math.factorial(5)
+print(fact)
+
+gc = math.gcd(24,56)
+print(gc)
